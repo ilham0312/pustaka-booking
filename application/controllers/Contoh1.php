4bc885b2ac2cbc9459734815b0f523da90aea7a3
ilham0312 <ramdaniilham40@gmail.com>
2021-10-18 00:15:37 +0700

    Upload awal project

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Ilham Ramdani
+              saya seorang Mahasiswa Universitas Bina Sarana Informatika";
+        echo "Saya jurusan Sistem Infomasi";
+    }
+}
