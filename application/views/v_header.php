6e904055ebefea94aef7ca4d19d07cde6637dc6d
ilham0312 <ramdaniilham40@gmail.com>
2021-10-28 16:41:37 +0700

    Upload awal project

---
+++ application/views/v_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-g">
+    <title>Web Prog II | Merancang Template sederhana dengan codeigniter
+    </title>
+    <link rel="stylesheet" type="text/css" href="<?php echo
+base_url() ?>assets/css/stylebuku.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RewntalBuku.net</h1>
+                <h3>Membuat Template sederhana dengan codeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url() . 'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url() . 'index.php/web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
