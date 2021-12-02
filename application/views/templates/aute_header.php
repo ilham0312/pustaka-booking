9d59dbd741286d1f9812649dea5ff18e969c7b6d
ilham0312 <ramdaniilham40@gmail.com>
2021-12-02 15:47:10 +0700

    PERTEMUAN 9

--- application/views/templates/aute_header.php
+++ application/views/templates/aute_header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>WP Univ. BSI | <?= $Judul; ?></title>
+    <title>pustaka booking | <?= $Judul; ?></title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
